d2ee132837c05b91a8d0ee1b88b53e22bc117e0b
sunghwan <tjdghks130907@gmail.com>
2024-10-03 13:46:31 +0900

    [백준 - 15664번] [Silver 2] N과 M 10 - 문제집 - 시리즈 - N과 M - 백트래킹 - 재귀함수를 활용한 문제 풀이

---
+++ src/main/java/wanted/baekjoon/P15664.java
@@ -0,0 +1,87 @@
+package main.java.wanted.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P15664 {
+    /**
+     * [문제]
+     * N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+     * - N개의 자연수 중에서 M개를 고른 수열
+     * - 고른 수열은 비내림차순이어야 한다.
+     * - 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+     *
+     * [입력]
+     * 첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+     * 둘째 줄에 N개의 수가 주어진다.
+     * 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+     *
+     * [출력]
+     * 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다.
+     * 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+     * 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+     *
+     * [예시]
+     * 입력 - 3 1                     출력 - 2
+     *       4 4 2                        4
+     *
+     * 입력 - 4 2                     출력 - 1 7
+     *      9 7 9 1                       1 9
+     *                                    7 9
+     *                                    9 9
+     *
+     * 입력 - 4 4                     출력 - 1 1 2 2
+     *       1 1 2 2
+     */
+
+    static int n;
+    static int m;
+    static int[] arr;   // 입력받은 수를 담을 배열
+    static int[] pick;  // 선택된 수를 담을 배열
+    static boolean[] check; // 선택되었는지 체크할 배열
+
+    public static void recursive(int depth) {
+        if (m == depth) {
+            for (int i : pick) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            int prev = 0;
+
+            for (int i = 0; i < n; i++) {
+                int prevIndexNumber = depth > 0 ? pick[depth - 1] : arr[i];
+                // 선택된 수가 아니면서 비내림차순을 만족하면서 중복된 수열이 아닌 경우
+                if (!check[i] && arr[i] >= prevIndexNumber && prev != arr[i]) {
+                    check[i] = true;
+                    pick[depth] = arr[i];
+                    prev = arr[i];
+                    recursive(depth + 1);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = reader.readLine().split(" ");
+        n = Integer.parseInt(nm[0]);
+        m = Integer.parseInt(nm[1]);
+        arr = new int[n];
+        check = new boolean[n];
+        pick = new int[m];
+
+
+        String[] nums = reader.readLine().split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(nums[i]);
+        }
+
+        Arrays.sort(arr);
+
+        recursive(0);
+    }
+}
